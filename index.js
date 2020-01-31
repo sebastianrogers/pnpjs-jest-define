1e5a81d7cff0bf4e776458389e4ab591058e0b17
Sebastian Rogers <rogerss@simpleinnovation.co.uk>
2020-01-31 12:05:31 +0000

    Modified to allow defines to work.

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-const pnp = require("@pnp/sp-commonjs");
-const client = require("@pnp/nodejs-commonjs");
+// Needed to allow define to work.
+require("amd-loader");
 
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
-
-// we call setup to use the node client
-pnp.sp.setup({
-  sp: {
-    fetchClientFactory: () => {
-      return new client.SPFetchClient(
-        process.env.SP_URL,
-        process.env.SP_CLIENT_ID,
-        process.env.SP_CLIENT_SECRET
-      );
-    }
+define(["@pnp/sp-commonjs", "@pnp/nodejs-commonjs"], (pnp, client) => {
+  if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
   }
+
+  // we call setup to use the node client
+  pnp.sp.setup({
+    sp: {
+      fetchClientFactory: () => {
+        return new client.SPFetchClient(
+          process.env.SP_URL,
+          process.env.SP_CLIENT_ID,
+          process.env.SP_CLIENT_SECRET
+        );
+      }
+    }
+  });
+
+  async function makeRequest() {
+    // make a request to get the web's details
+    const w = await pnp.sp.web();
+    console.log(JSON.stringify(w, null, 2));
+  }
+
+  // get past no await at root of app
+  makeRequest();
 });
-
-async function makeRequest() {
-  // make a request to get the web's details
-  const w = await pnp.sp.web();
-  console.log(JSON.stringify(w, null, 2));
-}
-
-// get past no await at root of app
-makeRequest();
